bc16b51ebc88fd0f36685ba53d5bc20d12bc4346
KGCraig <128436521+KGCraig@users.noreply.github.com>
2026-08-07 16:01:46 -0400

    Deploy Astro website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kgcraig.github.io/',
+  base: '/KGCraig.github.io',
+})
