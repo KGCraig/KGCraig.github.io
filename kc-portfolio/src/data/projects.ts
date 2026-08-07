27c1a2a54244fe8cb01014a325450b802e0abadc
KGCraig <128436521+KGCraig@users.noreply.github.com>
2026-08-07 13:18:45 -0400

    Improve layouts and styling
    - Added easier method for changing titles between pages
    - Cleaned up Featured Projects to make the layout more dynamic for different number of items inside the grid
    - Updated logo
    - Updated project typescript file to focus on one project at a time
    - Added SS image for RFM project (not yet implemented)
    - Removed unecessary <head> elements from pages and added them to base layout to improve consistency
    - Updated RFM project page (still WIP)

--- kc-portfolio/src/data/projects.ts
+++ kc-portfolio/src/data/projects.ts
@@ -2,22 +2,22 @@ export const projects = [
   {
     title: "RFM Customer Segmentation",
     description: "Customer segmentation using RFM scoring.",
-    image: "/images/rfm.png",
+    image: "",
     tech: ["Python", "Pandas", "Plotly", "Streamlit"],
-    link: "https://github.com/yourrepo",
+    link: "https://github.com/KGCraig/RFM-Analysis",
   },
-    {
-    title: "Speedrun API",
-    description: "Analyzing speedrun data.",
-    image: "/images/speedrunapi.png",
-    tech: ["Python", "Fast API", "Jinja2 + HTML", "DuckDB", "Pydantic"],
-    link: "https://github.com/yourrepo",
-  },
-    {
-    title: "Pikmin Webscrape",
-    description: "Collected data from website.",
-    image: "/images/webscrape.png",
-    tech: ["Python", "BeautifulSoup4", "Parquet"],
-    link: "https://github.com/yourrepo",
-  },
+  //   {
+  //   title: "Tiny Image Resizer CLI",
+  //   description: "",
+  //   image: "/images/speedrunapi.png",
+  //   tech: ["Python", "Fast API", "Jinja2 + HTML", "DuckDB", "Pydantic"],
+  //   link: "https://github.com/yourrepo",
+  // },
+  //   {
+  //   title: "Pikmin Webscrape",
+  //   description: "Collected data from website.",
+  //   image: "/images/webscrape.png",
+  //   tech: ["Python", "BeautifulSoup4", "Parquet"],
+  //   link: "https://github.com/yourrepo",
+  // },
 ];
