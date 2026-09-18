6d183ed1e17accf36a11d451a1bd4b0c14441a7f
KGCraig <128436521+KGCraig@users.noreply.github.com>
2026-09-18 03:31:04 -0400

    Update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kgcraig.github.io/',
-  base: '/KGCraig.github.io',
+  site: 'https://kgcraig.dev/',
 })
